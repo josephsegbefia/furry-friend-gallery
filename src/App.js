ad5f22968d08a42b41af54db039e406a3bc5fa09
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-26 16:30:43 +0000

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from "react";
-import DogCardInfo from "./DogInfoCard";
+import React, { useState, useEffect } from 'react';
+
+// components
+import DogCardInfo from './DogInfoCard';
+
+// styles
 import './App.css';
 
-
 const loadDogPictures = async (dogsToLoad = 8) => {
-  const apiBaseUrl = "https://dog.ceo/api/breeds/image/random";
+  const apiBaseUrl = 'https://dog.ceo/api/breeds/image/random/';
   const response = await fetch(`${apiBaseUrl}${dogsToLoad}`);
   const data = await response?.json();
-
   const dogData = data.message.map(item => {
-    let breed = item.replace("https://", "").split("/")[2];
+    // item is an image url in the format:
+    // "https://images.dog.ceo/breeds/bullterrier-staffordshire/n02093256_1727.jpg",
+    let breed = item.replace('https://', '').split('/')[2];
 
-    if (breed && breed !== ""){
-      breed = breed.split("-").reverse().join(" ");
+    // bread clean up (i.e. starts in the format 'subtype-maintype')
+    if (breed && breed !== '') {
+      breed = breed.split('-').reverse().join(' ');
     }
+
     return {
       id: `dog_pic_${data.message.indexOf(item)}`,
       imgUrl: item,
       breed
-    }
-  })
+    };
+  });
 
   return dogData;
 };
@@ -27,20 +33,19 @@ const loadDogPictures = async (dogsToLoad = 8) => {
 function App() {
   const [dogPictures, setDogPictures] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [numOfDogs, setNumOfDogs] = useState("");
+  const [numOfDogs, setNumOfDogs] = useState('');
   const [totalDogsSearched, setTotalDogsSearched] = useState(0);
 
-  const handleSubmit = async(event) => {
-    event.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault();
     setIsLoading(true);
     setDogPictures(await loadDogPictures(numOfDogs));
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
     setTotalDogsSearched(totalDogs => totalDogs + dogPictures.length);
-  }, [dogPictures])
-
+  }, [dogPictures]);
 
   useEffect(() => {
     (async () => {
@@ -49,11 +54,58 @@ function App() {
       setDogPictures(dogDataArray => [...dogPictureData]);
       setIsLoading(loading => !loading);
     })();
-  }, [])
+  }, []);
+
   return (
-    <>
-      <h1>Welcome to furry friends gallery</h1>
-    </>
+    <div className='container'>
+      <header className='columns section has-text-centered'>
+        <div className='column is-6 is-offset-3'>
+          <h1 className='title is-size-3'>
+            Search for pictures of good doggos
+          </h1>
+          <form className='form' onSubmit={handleSubmit}>
+            <div className='field has-addons has-addons-centered'>
+              <div className='control is-expanded'>
+                <input
+                  type='text'
+                  className='input is-medium'
+                  placeholder='how many dogs should we look for (max 50)?'
+                  value={numOfDogs}
+                  onChange={e => setNumOfDogs(e.target.value)}
+                />
+              </div>
+              <div className='control'>
+                <button className='button is-primary is-medium'>
+                  <span className='icon is-small'>
+                    <i className='fas fa-search'></i>
+                  </span>
+                  <span>search</span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </header>
+      <hr />
+      <div className='has-text-centered'>
+        <h3 className='subtitle is-size-4'>
+          All time puppers found = {totalDogsSearched}
+        </h3>
+      </div>
+      <div className='columns section is-multiline'>
+        {isLoading && (
+          <progress className='progress is-medium is-link' max='100'>
+            60%
+          </progress>
+        )}
+        {!isLoading &&
+          dogPictures.map(dogPicture => (
+            <div className='column is-one-quarter' key={dogPicture.id}>
+              <DogCardInfo {...dogPicture} />
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
 
--- src/DogInfoCard.jsx
+++ src/DogInfoCard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const DogCardInfo = ({ imgUrl, breed }) => {
   return (
-    <div className = "card dog-card">
-      <div classNAme = "card-image">
-      <figure className = "image" style = {{backgroundImage: `url(${imgUrl})`}}>
-        <img src = {imgUrl} alt = {`A ${breed} dog`} className = "is-sr-only" />
+    <div className='card dog-card'>
+    <div className='card-image'>
+      <figure className='image' style={{ backgroundImage: `url(${imgUrl})` }}>
+        <img src={imgUrl} alt={`A ${breed} dog`} className='is-sr-only' />
       </figure>
-      </div>
-      <div className = "card-content">
-        <div className = "content">
-          <strong>breed:</strong> {breed}
-        </div>
+    </div>
+    <div className='card-content'>
+      <div className='content'>
+        <strong>breed:</strong> {breed}
       </div>
     </div>
+  </div>
   )
 }
 
